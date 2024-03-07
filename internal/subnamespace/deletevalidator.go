ef6463bdaa9948cb182cc93ba99b92e946cddebc
dvir <dvir@dana.org>
2024-03-07 07:45:17 +0000

    Added validation logic on subnamespace delete Fixes #112 This PR introduces additional logic to the subnamespace validating webhook. From now on, only the hns service account can delete a subnamespace, everything else will be denied automatically.

---
+++ internal/subnamespace/deletevalidator.go
@@ -0,0 +1,24 @@
+package subnamespace
+
+import (
+	"fmt"
+	danav1 "github.com/dana-team/hns/api/v1"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// handleDelete implements the logic for the deletion of a subnamespace
+func (v *SubnamespaceValidator) handleDelete(req admission.Request) admission.Response {
+
+	response := v.validateServiceAccount(req.UserInfo.Username)
+	return response
+}
+
+// validateServiceAccount validates that the account requesting the deletion of a subnamespace
+// is the service account of the sns operator. Otherwise it will deny the request
+func (v *SubnamespaceValidator) validateServiceAccount(userName string) admission.Response {
+	if userName != fmt.Sprintf("system:serviceaccount:%s:%s", danav1.SNSNamespace, danav1.SNSServiceAccount) {
+		return admission.Denied(fmt.Sprintf("%s is not allowed to delete subnamespaces", userName))
+
+	}
+	return admission.Allowed("")
+}
